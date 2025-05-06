64a193b8df804e19209f4c02876ff5212cbfbede
noname6975 <cryptopochta6@gmail.com>
2025-05-06 13:58:20 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,10 +1,10 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
-void create_file(const char *filename);
-void delete_file(const char *filename);
-void write_to_file(const char *filename, const char *content);
-void read_file(const char *filename);
-int file_exists(const char *filename);
+FILE* open_fs_file(const char* filename);
+char* view_file_from_fs(FILE* fs, const char* target_filename);
+void delete_file_from_fs(FILE* fs, const char* target_filename);
+void add_new_file(FILE* fs, const char* filename, const char* content);
+void modify_file(FILE* fs, const char* filename, const char* new_content);
 
 #endif // FILESYSTEM_H
