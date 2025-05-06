26594957be563334f9abe3ba8b3c2ca55322d763
noname6975 <cryptopochta6@gmail.com>
2025-05-06 13:36:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "filesystem.h"
 
+int main() {
+    const char *fname = "test.txt";
+
+    create_file(fname);
+    write_to_file(fname, "Привет, мир!\n");
+    printf("Содержимое файла:\n");
+    read_file(fname);
+
+    return 0;
+}
